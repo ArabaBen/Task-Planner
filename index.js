43b44dc9f59ac52a9c1fd22006c29a517be3c08e
Araba <arabaocran28@gmail.com>
2022-08-10 16:23:06 -0400

    Updated index.js
    js file:
    current date of app use
    
    TO DO NEXT:
    Add and delete functions to buttons

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
-document.getElementById("myBtn").addEventListener("click", displayAdded);
+//document.getElementById("myBtn").addEventListener("click", displayAdded);
+//
+//function displayAdded() {
+//     
+//}
 
-function displayAdded() {
-     
-}
 
+
+//
+////PICKING ELETEMENTS
+//const clear = document.querySelector(".clear");
+//const dataElement = document.getElementById("date");
+//const list = document.getElementById("list");
+//const input = document.getElementById("input");
+//
+//
+//
+////SETTING CLASSES TO CONST "THIS  = THIS"
+//const DONE = "btn btn-primary";
+//const LINE_THROUGH = "lineThrough";
+//
+
+
+
+
+
+
+//THE CURRENT DATE 
+const d = new Date();
+document.getElementById("date").innerHTML = d.toDateString();
+
+
+
+
+
+//PUSH/ DELETE ITEAM for each card
+const card1 = document.querySelector("#card1");
+const btn = document.querySelector("button");
+    btn.addEventListener("click", function () {
+        card1.style.display = "none";
+    });
+    
 
 
 
